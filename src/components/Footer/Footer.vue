56f9e30a7ad7272f8719dda054b30dd684d9688e
n8dj88 <n8dj88@hotmail.com>
2026-06-02 20:49:44 +0800

    Update Footer.vue

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -23,11 +23,7 @@
         </p>
       </section>
       <section class="flex flex-col items-center gap-2">
-        <div class="flex gap-6">
-          <a title="打赏站长" target="_blank" href="/reward.webp">
-            <img class="w-8" src="@/assets/images/footer/shang.svg" />
-          </a>
-        </div>
+ 
         <span class="w-full font-bold text-lg text-center">图屋图床</span>
       </section>
     </main>
