b4873ed2b1934a8fb9ab403141a573c2466e5f74
n8dj88 <n8dj88@hotmail.com>
2026-07-02 03:16:06 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,5 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-
-// 基础样式
-import '@/assets/less/main.less'
-// tailwind-base
-import '@/assets/tailwind-base.css'
-
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+// 1. Tailwind基础底层
+import './assets/tailwind-base.css'
+// 2. 全局Less公共样式
+import './assets/less/main.less'
+// 其他代码...
